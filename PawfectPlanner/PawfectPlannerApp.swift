2851d93654aabfb94631207ccc8d600632d0fa2b
Bushra Bendou <bushrabendou@Bushras-MacBook-Pro.local>
2025-03-01 16:22:22 +0300

    Initial commit: Setup PawfectPlanner

---
+++ PawfectPlanner/PawfectPlannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PawfectPlannerApp.swift
+//  PawfectPlanner
+//
+//  Created by Bushra Bendou on 01/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct PawfectPlannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PawfectPlannerTests/PawfectPlannerTests.swift
@@ -0,0 +1,17 @@
+//
+//  PawfectPlannerTests.swift
+//  PawfectPlannerTests
+//
+//  Created by Bushra Bendou on 01/03/2025.
+//
+
+import Testing
+@testable import PawfectPlanner
+
+struct PawfectPlannerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
